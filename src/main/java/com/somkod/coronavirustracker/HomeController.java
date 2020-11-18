ccc5b4b1f25e5a7c4ff3fa917ac921f48ac91883
Soumy47 <soumya11.kol@gmail.com>
2020-11-18 19:15:50 +0530

    First Commit

---
+++ src/main/java/com/somkod/coronavirustracker/HomeController.java
@@ -0,0 +1,26 @@
+package com.somkod.coronavirustracker;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	CoronaVirusDataService coronaVirisDataService = new CoronaVirusDataService();  
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		List<LocationStats> allStats = coronaVirisDataService.getAllStats();
+		int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+		int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+		model.addAttribute("locationStats", allStats );
+		model.addAttribute("totalReportedCases", totalReportedCases );
+		model.addAttribute("totalNewCases", totalNewCases );
+		return "home";
+	}
+}
